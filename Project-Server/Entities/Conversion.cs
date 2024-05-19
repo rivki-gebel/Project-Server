f72ea084525b9a1bf18abdd724fc8a55765b37c9
rivki-gebel <148455850+rivki-gebel@users.noreply.github.com>
2024-05-19 12:14:00 +0300

    adding entities classes

---
+++ Project-Server/Entities/Conversion.cs
@@ -0,0 +1,9 @@
+﻿namespace Project_Server.Entities
+{
+    public class Conversion
+    {        public DateTime Time_last_update_utc { get; set; }
+        public DateTime Time_next_update_utc { get; set; }
+        public string Base_code { get; set; }
+        public Dictionary<string, double> Conversion_rates { get; set; } = new Dictionary<string, double>();
+    }
+}
---
+++ Project-Server/Entities/CurrenciesList.cs
@@ -0,0 +1,7 @@
+﻿namespace Project_Server.Entities
+{
+    public class CurrenciesList
+    {
+        public List<string[]> Supported_codes { get; set; }
+    }
+}
